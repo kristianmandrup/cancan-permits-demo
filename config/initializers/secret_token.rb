400aa5280d90e2ddb96534657f4aab800cee2f94
Kristian Mandrup <kmandrup@gmail.com>
2010-11-24 22:54:52 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CancanPermitsDemo::Application.config.secret_token = '61489015294a424e69846d0ff742a80d71c97037f2ef7b2f200676b662b67f70e059a8dee7ec119f934c93b837ccee37f0b4f8b386b5271da7af2402c05291f3'
